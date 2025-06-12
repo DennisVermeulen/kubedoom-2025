046424ad7c1a1436c4c55849f5841101f478d0d2
Dennis Vermeulen <d.a.m.vermeulen@gmail.com>
2025-06-12 14:08:31 +0200

    Fixes

--- scripts/game-timer.sh
+++ scripts/game-timer.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
 curl -q -s localhost:32001/resettimer > /dev/null
 
 kubectl config set-context --current --namespace=monster > /dev/null
 
-echo "Game On ...!"
-#sleep 60
-sleep 300
+# totale wachttijd in seconden
+TOTAL=300
+STEP=10
 
+echo "Game On ...!"
+
+# Countdown
+for ((remain=TOTAL; remain>0; remain-=STEP)); do
+    printf "\rNog %3d seconden...  " "$remain"   # overschrijft dezelfde regel
+    sleep "$STEP"
+done
+echo -e "\rNog   0 seconden...  "                # sluit de teller netjes af
 echo "Game Over....."
+
 
 open ${SCRIPT_DIR}/gameover.png 
 
@@ -21,7 +29,7 @@ echo "Your score is : $SCORE"
 sleep 10
 echo "Reset, get ready for a new game"
 
-#git reset --hard HEAD
-#git clean -f -d
+git reset --hard HEAD
+git clean -f -d
 
 ${SCRIPT_DIR}/reset.sh
